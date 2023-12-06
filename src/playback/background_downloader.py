86d82d5795b44a668d6b7878c8aaa433e7f16739
lixinghe1999 <helixing99@gmail.com>
2023-12-06 02:54:56 +0000

    reorganize into 3 folders

--- src/playback/background_downloader.py
+++ src/playback/background_downloader.py
@@ -1,8 +1,4 @@
 from audioset_download import Downloader
-labels = ["Door", "Cupboard open or close", "Drawer open or close", "Dishes, pots, and pans", "Cutlery, silverware", "Chopping (food)", "Frying (food)", 
-          "Microwave oven", "Blender", "Kettle whistle", "Water tap, faucet", "Sink (filling or washing)", "Bathtub (filling or washing)", "Hair dryer",
-          "Toilet flush", "Toothbrush", "Vacuum cleaner", "Zipper (clothing)", "Velcro, hook and loop fastener", "Keys jangling", "Coin (dropping)",
-          "Packing tape, duct tape", "Scissors", "Electric shaver, electric razor", "Shuffling cards", "Typing", "Writing"]
-d = Downloader(root_path='evaluation', labels=labels, n_jobs=4,
-                download_type='Evaluation', copy_and_replicate=False)
-d.download(format = 'vorbis')
+labels = ["Inside, small room", "Inside, large room or hall", "Inside, public space"]
+d = Downloader(root_path='evaluation', labels=labels, n_jobs=4, download_type='eval', copy_and_replicate=False)
+d.download(format = 'flac')
